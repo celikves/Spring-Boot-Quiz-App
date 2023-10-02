b8d7801c95c458fb4b5fc42fa0271800c76ac564
Vesile <vesile_emre@hotmail.com>
2023-10-02 16:55:26 +0300

    question service updated so that all questions returned from postgresql

--- src/main/java/com/vesile/quizapp/service/QuestionService.java
+++ src/main/java/com/vesile/quizapp/service/QuestionService.java
@@ -1,7 +1,19 @@
 package com.vesile.quizapp.service;
 
+import com.vesile.quizapp.Question;
+import com.vesile.quizapp.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+    public List<Question> getAllQuestions() {
+        return questionDao.findAll();
+    }
+
 }
