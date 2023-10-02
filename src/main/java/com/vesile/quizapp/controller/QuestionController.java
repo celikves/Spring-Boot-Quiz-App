ea0bc7eb54557c53ea31978391744a9d6b366e94
Vesile <vesile_emre@hotmail.com>
2023-10-02 18:13:36 +0300

    getting questions by category and adding new question end points created

--- src/main/java/com/vesile/quizapp/controller/QuestionController.java
+++ src/main/java/com/vesile/quizapp/controller/QuestionController.java
@@ -4,9 +4,7 @@ package com.vesile.quizapp.controller;
 import com.vesile.quizapp.Question;
 import com.vesile.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,7 +16,17 @@ public class QuestionController {
     QuestionService questionService;
 
     @GetMapping("allQuestions")
-    public List<Question>  allQuestions(){
+    public List<Question>  getAllQuestions(){
         return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable  String category){
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
     }
 }
