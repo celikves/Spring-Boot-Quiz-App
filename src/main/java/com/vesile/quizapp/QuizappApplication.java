7883194f5882eb582e0c2cb7bbfdcd475e2fcc46
Vesile <vesile_emre@hotmail.com>
2023-10-04 17:47:17 +0300

    missing entity scan added

--- src/main/java/com/vesile/quizapp/QuizappApplication.java
+++ src/main/java/com/vesile/quizapp/QuizappApplication.java
@@ -2,8 +2,10 @@ package com.vesile.quizapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.vesile.quizapp.model")
 public class QuizappApplication {
 
 	public static void main(String[] args) {
